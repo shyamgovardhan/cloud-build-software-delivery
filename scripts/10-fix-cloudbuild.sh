248cb9bef9a14d9697b7248d351c6b1eff91aa1b
Shyam Govardhan <shyamg@google.com>
2023-07-19 11:28:13 +1000

    10-fix-cloudbuild.sh

--- scripts/10-fix-cloudbuild.sh
+++ scripts/10-fix-cloudbuild.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
-CLOUDBUILD_YAML=../cloudbuild.yaml
-sed -i "s/<ZONE>/$ZONE/g" ${CLOUDBUILD_YAML}
-sed -i "s/<REGION>/$REGION/g" ${CLOUDBUILD_YAML}
+cbsd="./00-cbsd-init.sh"
+[[ ! -f ${cbsd} ]] && echo "${cbsd}: file not found!" && exit
+. ${cbsd}
+
+cd ..
+CLOUDBUILD_YAML=./cloudbuild.yaml
+sed -i "s/<ZONE>/${ZONE}/g" ${CLOUDBUILD_YAML}
+sed -i "s/<REGION>/${REGION}/g" ${CLOUDBUILD_YAML}
 sed -i "s/<PROJECT\_ID>/${PROJECT_ID}/g" ${CLOUDBUILD_YAML}
 sed -i "s/<REPO>/${PROJECT_ID}-repo/g" ${CLOUDBUILD_YAML}
 sed -i "s/<IMAGE>/image/g" ${CLOUDBUILD_YAML}
+cd -
